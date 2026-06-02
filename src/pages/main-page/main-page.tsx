295ec0473454eb310afba00a74d554286a68802a
Pavel-Surkov <surkovv.pavel@gmail.com>
2026-06-03 02:57:58 +0700

    feat(main-page): add centered placeholder content

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,7 +1,11 @@
 import { FC } from 'react'
 
 export const MainPage: FC = () => {
-  return <div className="relative h-[var(--full-height)]"></div>
+  return (
+    <div className="relative h-(--full-height) flex items-center justify-center">
+      <p>Hola, world!</p>
+    </div>
+  )
 }
 
 export default MainPage
